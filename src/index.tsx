7b83d1de9dc3f25ce97e4b49106b96bbff0a1b8d
Walter <silvawalteralipio@gmail.com>
2022-07-05 11:05:55 -0300

    lidando com arquivos estaticos

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
 import './index.css';
+import Cardapio from './pages/Cardapio';
 
 
 
@@ -9,7 +11,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div> Aluroni </div>
+    <Cardapio />
   </React.StrictMode>
 );
 
